c5a496db590603b888c0ddba110537f8f36a9a09
prachipundir29 <prachipundir64@gmail.com>
2026-01-28 14:49:10 +0530

    Connect ATo Database

--- src/db/index.js
+++ src/db/index.js
@@ -1,24 +1,35 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB = async () => {
 
+const connectDB = async ()=> {
     try {
-      const connectionInstance = await mongoose.connect
-      (`${process.env.MONGODB_URI }/${ DB_NAME}`)
-      console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} \n`);
-       
+      const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
+      console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+      
     } catch (error) {
-        console.log("MONGODB connection error:", error);
-        process.exit(1)
-        
+      console.log("MONGODB connection error ", error);
+       process.exit(1) 
     }
+}
+// const connectDB = async () => {
+
+//     try {
+//       const connectionInstance = await mongoose.connect
+//       (`${process.env.MONGODB_URI }/${ DB_NAME}`)
+//       console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} \n`);
+       
+//     } catch (error) {
+//         console.log("MONGODB connection error:", error);
+//         process.exit(1)
+        
+//     }
 
 //   mongoose.connection.on('connected', () => {
 //     console.log("MongoDB connected successfully");    
 //   })
 //   await mongoose.connect(`${process.env.MONGODB_URI}/videoTube`)
-}
+// }
 
 
 export default connectDB;
